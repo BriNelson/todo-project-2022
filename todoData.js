fa62a2e9a15c1c0048cfc74991b025741bb7eeec
Brinelson <ladofmusic@gmail.com>
2022-09-22 22:52:25 -0600

    todo data model

---
+++ todoData.js
@@ -0,0 +1,45 @@
+
+// name
+//completed
+//id
+//category
+//due Date
+
+
+let todoData = [
+    {
+    taskName: 'test',
+    completed: false,
+    id: 223,
+    category: 'exercise',
+    dueDate: 'dateObj'
+    },
+    
+
+    {
+        taskName: 'homework',
+        completed: false,
+        id: 223,
+        category: 'exercise',
+        dueDate: 'dateObj'
+    
+    },
+    {
+        taskName: 'work out',
+        completed: false,
+        id: 223,
+        category: 'exercise',
+        dueDate: 'dateObj'
+    
+    },
+    
+    
+]
+
+const addTodo = () => {}
+const deleteTask = () => { }
+const completeTask = () => { }
+const saveTask = () => { }
+const deleteAllTask = () => { }
+const editTask = () => { }
+const editDueDate = () => { }
