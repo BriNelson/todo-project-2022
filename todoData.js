87d91b4522c8745f9e83c4596000b41202180b4b
Brinelson <ladofmusic@gmail.com>
2022-10-03 15:37:52 -0600

    added list showing mechanics

--- todoData.js
+++ todoData.js
@@ -1,45 +1,123 @@
-
 // name
 //completed
 //id
 //category
 //due Date
 
+//* User can view todos
+
+//* User can add todos
+
+//* User can edit todos
+
+//* User can delete todos
+
+//* App shows the user number of todos left to complete
+
+//* App can delete (aka clear) all done todos at once.
 
 let todoData = [
-    {
-    taskName: 'test',
+  {
+    taskName: "test",
     completed: false,
     id: 223,
-    category: 'exercise',
-    dueDate: 'dateObj'
-    },
+    category: "exercise",
+    dueDate: "dateObj",
+  },
+
+  {
+    taskName: "homework",
+    completed: false,
+    id: 223,
+    category: "exercise",
+    dueDate: "dateObj",
+  },
+  {
+    taskName: "work out",
+    completed: false,
+    id: 223,
+    category: "exercise",
+    dueDate: "dateObj",
+  },
+];
+
+console.log(todoData);
+
+//Add todo button
+const addTodoButton = document.querySelector('#addTodoButton')
+
+addTodoButton.addEventListener("click", () => {
+    let newTask = {
+        taskName: document.querySelector("#userTask").value,
+        category: 'test',
+        id: 'test',
+        completed: false,
+        dueDate: 'test date'
+    }
+    
+    todoData.unshift(newTask);
+
+    console.log(newTask);
+    printTodoList(todoData)
+ })
+
+
+//print todo array function
+const printTodoList = (arr) => {
+    document.querySelector("#toDoList").innerHTML = "";
+  arr.forEach((element) => {
+    let todoList = document.querySelector("#toDoList");
+
+    const listItem = document.createElement("li");
+    todoList.appendChild(listItem);
+    // Adds div tag with box class to give shape
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("box", "level");
+    listItem.appendChild(itemDiv);
+
+    //div container for left side of list
+    const leftSideDiv = document.createElement("div");
+    leftSideDiv.classList.add("level-left")
+    itemDiv.appendChild(leftSideDiv);
+
+    
+    //todo item name
+    const itemName = document.createElement("h1");
+    itemName.appendChild(document.createTextNode(`${element.taskName}`));
+    itemName.classList.add('level-item')
+    leftSideDiv.appendChild(itemName);
+
+    //Div container for right side
+    const rightSideDiv = document.createElement("div");
+    rightSideDiv.classList.add("level-right")
+    itemDiv.appendChild(rightSideDiv);
+
+    // Edit button added
+    const editButton = document.createElement("button")
+    editButton.classList.add("button", "is-info", "level-item")
+    editButton.appendChild(document.createTextNode('edit'))
+    rightSideDiv.appendChild(editButton);
+    // Delete button added
+    const deleteButton = document.createElement("button")
+    deleteButton.classList.add("button", "is-danger", "level-item")
+    deleteButton.appendChild(document.createTextNode('delete'))
+    rightSideDiv.appendChild(deleteButton);
+    
+    
+    
     
 
-    {
-        taskName: 'homework',
-        completed: false,
-        id: 223,
-        category: 'exercise',
-        dueDate: 'dateObj'
     
-    },
-    {
-        taskName: 'work out',
-        completed: false,
-        id: 223,
-        category: 'exercise',
-        dueDate: 'dateObj'
-    
-    },
-    
-    
-]
 
-const addTodo = () => {}
-const deleteTask = () => { }
-const completeTask = () => { }
-const saveTask = () => { }
-const deleteAllTask = () => { }
-const editTask = () => { }
-const editDueDate = () => { }
+    console.log(element.taskName);
+  });
+};
+printTodoList(todoData);
+
+// const addTodo = () => {}
+// const deleteTask = () => { }
+// const completeTask = () => { }
+// const saveTask = () => { }
+// const deleteAllTask = () => { }
+// const editTask = () => { }
+// const editDueDate = () => { }
