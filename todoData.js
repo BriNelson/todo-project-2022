430e0267ab0432d9139d02f4f265f45af6aebe73
Brinelson <ladofmusic@gmail.com>
2022-11-14 00:57:59 -0700

    everything working except concurent delete

--- todoData.js
+++ todoData.js
@@ -20,28 +20,13 @@
 // * The experience around categories must be a good user experience
 
 let todoData = [
-  {
-    taskName: "cats",
-    completed: false,
-    id: 1,
-    category: "exercise",
-    dueDate: "dateObj",
-  },
-
-  {
-    taskName: "homework",
-    completed: false,
-    id: 2,
-    category: "exercise",
-    dueDate: "dateObj",
-  },
-  {
-    taskName: "work out",
-    completed: false,
-    id: 3,
-    category: "exercise",
-    dueDate: "dateObj",
-  },
+  // {
+  //   taskName: "cats",
+  //   completed: false,
+  //   id: 1,
+  //   category: "exercise",
+  //   dueDate: "dateObj",
+  // },
 ];
 
 let categoryList = [
@@ -71,9 +56,12 @@ fullTodoList.addEventListener('click', (event) => {
 printedCategoryList.addEventListener("click", (event) => {
   if (event.target.matches(".deleteCategory")) {
     categoryList.splice(event.target.id, 1);
+    
     printCategories(categoryList)
-    console.log(event.target)
+   
+    
   }
+  console.log(event.target.id)
 
 })
 
@@ -161,7 +149,6 @@ document.addEventListener("click", function (event) {
 })
  
  // count function
-
 const countCompleted = (arr) => {
   document.querySelector("#completedTaskCount").innerHTML = "";
   let counter = document.querySelector("#completedTaskCount")
@@ -231,32 +218,111 @@ const printTodoList = (arr) => {
     rightSideDiv.classList.add("level-right")
     itemDiv.appendChild(rightSideDiv);
 
+//////////////////////////////// Category section of todo list //////////////////////////////////////////////
+
+    
+  
+   
+    
+    
 // item category
     const categoryControl = document.createElement("div");
     categoryControl.classList.add("control")
-    rightSideDiv.appendChild(categoryControl);
-
+      rightSideDiv.appendChild(categoryControl);
+      if (element.category === ""){ categoryControl.remove()} // messy but keeps tag from repopulated on re-draw
+// Bulma css tag 
     const tagsObject = document.createElement("div")
-    tagsObject.classList.add("tags", "has-addons")
+    tagsObject.classList.add("tags", "has-addons", "mr-6")
     categoryControl.appendChild(tagsObject);
-
+// edit category on todo list
     const editCategoryButton = document.createElement("button");
     editCategoryButton.classList.add("tag", "button", "is-dark", "is-normal")
+    editCategoryButton.setAttribute('id', index)
     editCategoryButton.appendChild(document.createTextNode("edit"))
     tagsObject.appendChild(editCategoryButton);
+// save category on todo list
+    const saveCategoryButton = document.createElement("button");
+    saveCategoryButton.classList.add("tag", "button", "is-dark", "is-normal")
+    saveCategoryButton.setAttribute('id', index)
+    saveCategoryButton.appendChild(document.createTextNode("save"))
 
+    
+    
+// title for category on todo list
     const categoryTitle = document.createElement("span")
     categoryTitle.classList.add("tag", "is-primary", "is-normal")
-    categoryTitle.appendChild(document.createTextNode(`${element.category}`));
     tagsObject.appendChild(categoryTitle);
 
-    const deleteCategoryBtn = document.createElement("button")
-    deleteCategoryBtn.classList.add("delete", "is-small")
-    categoryTitle.appendChild(deleteCategoryBtn);
+    const categoryTitleSpan = document.createElement("span")
+    categoryTitleSpan.appendChild(document.createTextNode(`${element.category}`));
+    categoryTitle.appendChild(categoryTitleSpan)
+    
+    // edit category field on todo list
 
+    const editCategoryField = document.createElement("input")
+    editCategoryField.classList.add("input", "is-primary", "is-small", "is-rounded")
+
+    // delete category from todo list
+    const deleteCategoryBtn = document.createElement("button")
+    deleteCategoryBtn.classList.add("delete", "is-small", "deleteCategory")
+    deleteCategoryBtn.setAttribute('id', index)
+      categoryTitle.appendChild(deleteCategoryBtn);
+      
+    
+    deleteCategoryBtn.addEventListener("click", (event) => {
+      
+
+      todoData[index] = {
+        taskName: element.taskName,
+        completed: element.completed,
+        id: element.id,
+        category: "",
+        dueDate: element.dueDate
+       
+      }
+      printTodoList(todoData)
+      
+    })
+
+     // ─── Edit Category Listener On The Todo List Button ──────────
+
+     
+    editCategoryButton.addEventListener('click', (event) => {
+      editCategoryButton.replaceWith(saveCategoryButton)
+                  categoryTitleSpan.replaceWith(editCategoryField)
+       
+     })
+
+   // ─── Save Category Listener ──────────────────────────────────────────
+
+   
+    saveCategoryButton.addEventListener("click", (event) => {
+      // console.log(element.taskName)
+      // console.log(editField.value)
+
+      element.category = editCategoryField.value
+      saveCategoryButton.replaceWith(editCategoryButton)
+       
+      const index = categoryList.findIndex((el) => el.id === element.id)
+      console.log(index)
+
+      categoryList[index] = {
+        id: element.id,
+        categoryName: editCategoryField.value
+
+      }
+      
+      
+      printCategories(categoryList)
+      // editField.replaceWith(itemName)
+      printTodoList(todoData)
+      
+    })
 
 
     
+
+    ////////////////////////////////////// Edit Todo function ///////////////////////////////////////////////////
 
     //edit todo field 
     const editField = document.createElement("input")
@@ -268,6 +334,7 @@ const saveButton = document.createElement("button")
 saveButton.classList.add("button", "is-success", "level-item", "editBtn")
     saveButton.appendChild(document.createTextNode('save')) 
     
+    // Save button event for todo list - swaps save with edit button
     saveButton.addEventListener("click", (event) => {
       // console.log(element.taskName)
       // console.log(editField.value)
@@ -279,16 +346,16 @@ saveButton.classList.add("button", "is-success", "level-item", "editBtn")
       printTodoList(todoData)
     })
 
-    // Edit button added
+    // Edit button for todo list added
     const editButton = document.createElement("button")
     editButton.classList.add("button", "is-info", "level-item", "editBtn")
     editButton.setAttribute('id', index)
     editButton.appendChild(document.createTextNode('edit'))
     rightSideDiv.appendChild(editButton);
     
+    // Edit button event - saves edited item
     editButton.addEventListener('click', (event) => {
      editButton.replaceWith(saveButton)
-        //itemName.replaceWith(editField)
         itemName.replaceWith(editField)
       
     })
@@ -301,13 +368,13 @@ saveButton.classList.add("button", "is-success", "level-item", "editBtn")
     rightSideDiv.appendChild(deleteButton);
     
     
-    // console.log(element.taskName);
+    // console.log(element.taskName); 
   });
   countCompleted(arr)
 };
 printTodoList(todoData);
 
-// Print Categories
+////////////////// Print Category List ////////////////////////////
 const printCategories = (arr) => {
   document.querySelector("#categoryList").innerHTML = "";
   document.querySelector("#categoryDropdown").innerHTML = "";
